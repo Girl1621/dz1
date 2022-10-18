dfc41e2991ad4cbe752be219cf813f01516e3e83
Ksenia <Kseniakuzevanova@gmail.com>
2022-10-18 17:32:24 +0700

    firstcommit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿Console.WriteLine("Введите первое число:");
-   var number1 = Decimal.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число:");
+   var number1 = Convert.ToInt32(Console.ReadLine());
    Console.WriteLine("Введите второе число:");
-     var number2 = Decimal.Parse(Console.ReadLine());
+     var number2 = Convert.ToInt32(Console.ReadLine());
      Console.WriteLine("Большее число число:");
     if (number1 > number2)
       Console.WriteLine(number1);
