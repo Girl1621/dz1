f20890ad8bf36b6f10f8d4b97974c70d7086dda3
Ksenia <Kseniakuzevanova@gmail.com>
2022-10-18 17:29:36 +0700

    firstcommit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число:");
+   var number1 = Decimal.Parse(Console.ReadLine());
+   Console.WriteLine("Введите второе число:");
+     var number2 = Decimal.Parse(Console.ReadLine());
+     Console.WriteLine("Большее число число:");
+    if (number1 > number2)
+      Console.WriteLine(number1);
+    else 
+      Console.WriteLine(number2);
+
+ 
