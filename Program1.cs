0c05706d24ca4b78a5ba955a147fedf991f0f089
Ksenia <115340620+Girl1621@users.noreply.github.com>
2022-10-20 21:41:45 +0700

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,94 @@
+//Напишите программу, которая на вход принимает два числа и выдаёт,
+//какое число большее, а какое меньшее.
+
+//Console.WriteLine("Введите первое число:");
+  //var number1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Введите второе число:");
+  //var number2 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Большее число число:");
+
+  //if (number1 > number2)
+ //{
+ //   Console.WriteLine(number1);
+  //}
+
+  //else
+   //{
+    //Console.WriteLine(number2);
+   //}
+
+
+
+//максимальное из трех чисел
+
+//Console.WriteLine("Введите первое число:");
+  //var number1 = Decimal.Parse(Console.ReadLine());
+
+//Console.WriteLine("Введите второе число:");
+  //var number2 = Decimal.Parse(Console.ReadLine());
+
+//Console.WriteLine("Введите третье число:");
+  //var number3 = Decimal.Parse(Console.ReadLine());
+
+//Console.WriteLine("Большее число число:");
+
+  //if (number1 > number2 & number1 > number3)
+  //{
+  //  Console.WriteLine(number1);
+  //}
+
+  //else
+   //if (number2 > number1 & number2 > number3)
+
+  //{
+    //Console.WriteLine(number2);
+  //}
+
+  //else 
+  //{ 
+    //Console.WriteLine("Наибольшее число " + number3);
+
+  //}
+
+
+// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+  //Console.WriteLine("Введите число:");
+    //int number;
+    //number =  Convert.ToInt32(Console.ReadLine());
+    //int temp = number % 2;
+     //bool result = temp == 0;
+    
+    //if (result)
+    //{
+     //   Console.WriteLine("число четное");
+    //} 
+
+    //else
+    //{
+     //   Console.WriteLine("число нечетное");
+    //}
+
+
+//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+  Console.WriteLine("Введите число:");
+
+    int number;
+    number =  Convert.ToInt32(Console.ReadLine());
+    
+
+for
+(int EvenNumberCount = 2; EvenNumberCount < number; EvenNumberCount++)
+
+if 
+(EvenNumberCount%2==0)
+
+Console.WriteLine(EvenNumberCount);
+
+
+  
